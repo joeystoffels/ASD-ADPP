e2344f6015a25b244590625da677987d20cc843f
Joey Stoffels <joey.stoffels@exe.energy>
2019-09-26 23:20:16 +0200

    start of project

---
+++ src/main/java/asd/adpp/quicksort/QuickSort.java
@@ -0,0 +1,12 @@
+package asd.adpp.quicksort;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class QuickSort {
+
+    public void quickSort() {
+        log.warn("in quickSort()");
+    }
+
+}
---
+++ src/test/java/QuickSortTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class QuickSortTest {
+
+    @Test
+    public void quickSortTest() {
+        assertTrue(true);
+    }
+
+}
